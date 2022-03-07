dd25a61e434efaf41f3aadb51b39bbb1a3dc2de5
JONGHYUK LEE <2516328@hanmail.net>
2022-03-07 14:34:29 +0900

    .

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -28,18 +28,20 @@ public class Test2 {
 //		System.out.println(Createvo);
 		aaa.create(Createvo);// 내용 생성하기
 		
+		
+		
 		// 3. Read 내용 불러오기.(전체 내용을 가져오기)
-//		NoticeDAO aaa = new NoticeDAO();
-//		List<NoticeVO> list =  aaa.read();
-//		Iterator<NoticeVO> it = list.iterator();//배열 속에서 하나씩 꺼낼 수 있다.
-//		while(it.hasNext()) {
-//			NoticeVO VO = it.next();
-//			
-//			System.out.print(VO.getNo() + "\t");
-//			System.out.print(VO.getTitle() + "\t");
-//			System.out.print(VO.getContent() + "\t");
-//			System.out.println(VO.getInputDate() + "\t");
-//		}
+		NoticeDAO aaa = new NoticeDAO();
+		List<NoticeVO> list =  aaa.read();
+		Iterator<NoticeVO> it = list.iterator();//배열 속에서 하나씩 꺼낼 수 있다.
+		while(it.hasNext()) {
+			NoticeVO VO = it.next();
+			
+			System.out.print(VO.getNo() + "\t");
+			System.out.print(VO.getTitle() + "\t");
+			System.out.print(VO.getContent() + "\t");
+			System.out.println(VO.getInputDate() + "\t");
+		}
 
 		
 
